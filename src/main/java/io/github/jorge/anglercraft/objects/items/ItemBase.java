229f864c7df6f4eff72365d4a2cfd0b2abdfde86
JorgeFishkin <coletonkin@gmail.com>
2023-10-05 00:39:36 -0400

    add hook item

--- src/main/java/io/github/jorge/anglercraft/objects/items/ItemBase.java
+++ src/main/java/io/github/jorge/anglercraft/objects/items/ItemBase.java
@@ -3,7 +3,6 @@ package io.github.jorge.anglercraft.objects.items;
 import io.github.jorge.anglercraft.util.IHasModel;
 import io.github.jorge.anglercraft.AnglerCraft;
 import io.github.jorge.anglercraft.init.ItemInit;
-import io.github.jorge.anglercraft.proxy.ClientProxy;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
